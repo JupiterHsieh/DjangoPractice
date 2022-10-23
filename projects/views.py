4ec531f2e776876d2e168bff4a0c927d1c3ab259
user <user@sd213065.ming.ab.nthu.edu.tw>
2022-10-24 03:24:41 +0800

    render done, templates haven't

---
+++ projects/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def projects(request):
+    return render(request,'projects.html')
+
+def project(request, pk):
+    return render(request,'singles-projects.html')
+

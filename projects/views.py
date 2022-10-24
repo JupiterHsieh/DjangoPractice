28fac1ba71edbfa3e7b1d078dd160ef7385433f8
user <user@sd213065.ming.ab.nthu.edu.tw>
2022-10-24 23:17:44 +0800

    Section 2 done

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
+kkk =  [
+{
+    'id':1,
+    'title' : "E-commerce website",
+    'description' : 'E commerce telling '
+},
+    {
+    'id':2,
+    'title' : "Profolio website",
+    'description' : 'Profolio telling '
+},
+    {
+    'id':3,
+    'title' : "Social network website",
+    'description' : 'Social network telling '
+}
+]
+
 def projects(request):
-    return render(request,'projects.html')
+    page = 'projects'
+    number =10
+    context = {'page':page,'number':number,'projects':kkk}
+    return render(request,'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request,'singles-projects.html')
+    projectObj = None
+    for i in kkk:
+        if str(i['id']) == pk:
+            projectObj=i
+
+    return render(request,'projects/singles-projects.html',{'project':projectObj})
 
